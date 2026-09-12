3fbbbe18c18e20b63960efc176a4b1bfce5770f8
Kali Live user <stratospherestu@gmail.com>
2026-09-12 11:09:31 +0800

    feat(architecture): Move lib folder in its proper structure according to architecture.md

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -7,7 +7,7 @@ import { getStorage } from 'firebase-admin/storage';
 const firebaseConfig : admin.AppOptions = {
     credential : admin.cert(
         {
-            projectId : process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            projectId : process.env.FIREBASE_PROJECT_ID,
             clientEmail : process.env.FIREBASE_CLIENT_EMAIL,
             privateKey : process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         },
